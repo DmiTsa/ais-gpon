49f6cc23769b85400e60c6531b52f4911adcb9a8
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2023-01-17 13:50:28 +0300

    w1

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import sequelize from './db.js';
+dotenv.config();
+
+// require('dotenv').config();
+// const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
