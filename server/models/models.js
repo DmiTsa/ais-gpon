71094560f1a7912031442438142211d540c4c7a5
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2023-01-19 16:15:20 +0300

    w2

--- server/models/models.js
+++ server/models/models.js
@@ -24,3 +24,49 @@ const Order = sequelize.define('order', {
   orderDate: { type: DataTypes.DATE, allowNull: false },
   orderFile: { type: DataTypes.STRING },
 });
+
+const InventaryCable = sequelize.define('inventaryCable', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+});
+
+const InventaryEquip = sequelize.define('inventaryEquip', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Pol = sequelize.define('pol', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  info: { type: DataTypes.STRING },
+});
+
+const CableSection = sequelize.define('cableSection', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  length: { type: DataTypes.INTEGER },
+  lengthCanal: { type: DataTypes.INTEGER },
+  lengthWall: { type: DataTypes.INTEGER },
+  lengthGround: { type: DataTypes.INTEGER },
+});
+
+const Adress = sequelize.define('adress', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  street: { type: DataTypes.STRING, allowNull: false },
+  house: { type: DataTypes.STRING, allowNull: false },
+  apartment: { type: DataTypes.STRING },
+  building: { type: DataTypes.STRING },
+});
+
+const Cables = sequelize.define('cables', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  capacity: { type: DataTypes.INTEGER, allowNull: false },
+  descr: { type: DataTypes.STRING },
+});
+
+const CableUnit = sequelize.define('cableUnit', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoincrement: true },
+  type: { type: DataTypes.STRING, allowNull: false },
+  name: { type: DataTypes.STRING, allowNull: false },
+});
