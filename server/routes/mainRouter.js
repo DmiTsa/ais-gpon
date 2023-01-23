772b5b6c713de3e8476abdee40bb694906241256
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2023-01-23 11:02:28 +0300

    w1

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -6,7 +6,6 @@ router.use('/pol', router); //polRouter
 router.use('/inventary-cable', router); //inventaryCableRouter
 router.use('/inventary-equip', router); //inventaryEquipRouter
 router.use('/cable-section', router); //cableSectionRouter
-router.use('/cable-section', router); //cableSectionRouter
 router.use('/adress', router); //adressRouter
 router.use('/build-object', router); //buildObjectRouter
 router.use('/cable', router); //cableRouter
